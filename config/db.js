db97d192ba6ba09873832f7eff5f178f1c7b2c57
unknown <taboubioussema78@gmail.com>
2025-03-02 22:15:03 +0100

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://admin:784408@cluster0.p5brv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
